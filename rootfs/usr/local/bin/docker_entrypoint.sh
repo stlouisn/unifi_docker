5bedd597932b54c8dd5920d7b9adcda4ea41afd0
stlouisn <stlouisn@hotmail.com>
2017-07-03 18:31:23 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -7,23 +7,23 @@ echo $TZ > /etc/timezone
 
 # Make sure volumes are mounted correctly
 if [ ! -d /usr/lib/unifi/data ]; then
-  printf "\nERROR: volume /usr/lib/unifi/data not mounted.\n" >&2
-  exit 1
+    printf "\nERROR: volume /usr/lib/unifi/data not mounted.\n" >&2
+    exit 1
 fi
 
 # Copy default system.properties file
 if [ ! -e /usr/lib/unifi/data/system.properties ]; then
-  cp /etc/unifi/system.properties /usr/lib/unifi/data/.
+    cp /etc/unifi/system.properties /usr/lib/unifi/data/.
 fi
 
 # Create default site folder
 if [ ! -d /usr/lib/unifi/data/sites/default ]; then
-  mkdir -p /usr/lib/unifi/data/sites/default
+    mkdir -p /usr/lib/unifi/data/sites/default
 fi
 
 # Copy default config.properties file
 if [ ! -e /usr/lib/unifi/data/sites/default/config.properties ]; then
-  cp /etc/unifi/config.properties /usr/lib/unifi/data/sites/default/.
+    cp /etc/unifi/config.properties /usr/lib/unifi/data/sites/default/.
 fi
 
 # Fix user and group ownerships
@@ -34,7 +34,7 @@ cd /usr/lib/unifi
 
 # Start unifi in console mode
 exec gosu unifi \
-  /usr/bin/java \
-    "$@" \
-    -Djava.awt.headless=true \
-    -jar /usr/lib/unifi/lib/ace.jar start
+    /usr/bin/java \
+        "$@" \
+        -Djava.awt.headless=true \
+        -jar /usr/lib/unifi/lib/ace.jar start

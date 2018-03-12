cbe6bea22d25f6a6a5207294960611e7930cf895
stlouisn <stlouisn@hotmail.com>
2018-03-12 10:30:22 -0300

    Update docker_entrypoint.sh

--- rootfs/usr/local/bin/docker_entrypoint.sh
+++ rootfs/usr/local/bin/docker_entrypoint.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-# Make sure volumes are mounted correctly
+#=========================================================================================
+
+# Make sure volume '/usr/lib/unifi/data' is mounted and writeable
 if [[ ! -d /usr/lib/unifi/data ]]; then
-    echo -e "\nError: volume '/usr/lib/unifi/data/' not mounted.\n" >&2
+    echo -e "\nError: volume '/usr/lib/unifi/data' is not mounted.\n" >&2
     exit 1
 elif [[ `mount | grep '/usr/lib/unifi/data' | awk -F '(' {'print $2'} | cut -c -2` == "ro" ]]; then
-    echo -e "\nError: volume '/usr/lib/unifi/data/' is readonly.\n" >&2
+    echo -e "\nError: volume '/usr/lib/unifi/data' is readonly.\n" >&2
     exit 1
 fi
 
@@ -24,14 +26,17 @@ if [[ ! -e /usr/lib/unifi/data/sites/default/config.properties ]]; then
     cp /etc/unifi/config.properties /usr/lib/unifi/data/sites/default/.
 fi
 
-# Fix user and group ownerships
+# Fix user and group ownerships for '/usr/lib/unifi/data'
 chown -R unifi:unifi /usr/lib/unifi/data
+
+#=========================================================================================
 
 # Change workdir
 cd /usr/lib/unifi
 
 # Start unifi in console mode
-exec gosu unifi /usr/bin/java \
+exec gosu unifi \
+    /usr/bin/java \
     -Xmx1024m \
     -Djava.awt.headless=true \
     -jar /usr/lib/unifi/lib/ace.jar start
